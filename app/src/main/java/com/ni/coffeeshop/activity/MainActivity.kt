5e649167134ef9fa94acbacafa9079618c3cd5f4
Nasif Imtiaj <nasifimtiaj.16@gmail.com>
2023-07-14 02:36:35 +0600

    Initial commit.

---
+++ app/src/main/java/com/ni/coffeeshop/activity/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ni.coffeeshop.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ni.coffeeshop.databinding.MainActivityBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MainActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
